87ee4f5f8e6e1cc3e7eeb651906c17404f810141
Samson <57755235+Samson-DVS@users.noreply.github.com>
2020-04-29 12:35:12 +0800

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,12 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+addition = float(num1) + float(num2)
+sub = float(num1) - float(num2)
+multi = float(num1) * float(num2)
+divi = float(num1) / float(num2)
+
+print(addition)
+print(sub)
+print(multi)
+print(divi)
