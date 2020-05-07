7250974fd6aeda75ad322aa3a0725abec55cd070
Samson <57755235+Samson-DVS@users.noreply.github.com>
2020-05-07 13:36:30 +0800

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,13 @@
+print("*********************")
+print("Just a random Madlib")
+print("*********************")
+#collecting inpute from user
+color = input("Enter any color: ")
+noun = input("Enter any noun: ")
+plural_noun = input("Enter any plural noun: ")
+place = input("Enter any place: ")
+
+#Assigning variables
+print("Roses are " + color)
+print(noun + " are blue")
+print("Be kind to your " + plural_noun + " in " + place)
