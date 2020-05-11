2cd6fd5512aca9120878015b31e5d845467a6248
Samson <57755235+Samson-DVS@users.noreply.github.com>
2020-05-11 14:28:07 +0800

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -10,3 +10,19 @@ print(addition)
 print(sub)
 print(multi)
 print(divi)
+
+#Calculator improvements
+num1 = float(input("Enter the first number: "))
+op = input("Enter the operator: ")
+num2 = float(input("Enter the second number: "))
+
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "*":
+    print(num1*num2)
+elif op == "/":
+    print(num1/num2)
+else:
+    print("Invalid operator")
